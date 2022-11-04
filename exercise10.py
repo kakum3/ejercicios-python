13f300e1a02f09888b999da53c7e021cbe158c41
kakum3 <104210758+kakum3@users.noreply.github.com>
2022-11-04 11:16:29 +0100

    ejercicio 8.2

--- exercise10.py
+++ exercise10.py
@@ -1,3 +1,11 @@
 f = open('hola mundo.txt', 'w')
-f.write('Hola mundo')
+f.write('Hola mundo\n')
+f.close()
+
+f = open('hola mundo.txt', 'r+')
+f.readline()
+f.write('Esta es la segunda vez que escribo.\n')
+
+f.seek(0)
+print(f.read())
 f.close()
